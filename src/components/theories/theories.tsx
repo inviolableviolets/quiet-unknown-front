8d449e9464af8982e7aed6588ae8289f2dd1b879
Sergio Mata <sergiomataherrero@gmail.com>
2023-10-09 15:12:59 +0200

    Update theories section

--- src/components/theories/theories.tsx
+++ src/components/theories/theories.tsx
@@ -5,62 +5,58 @@ import { TiThSmallOutline, TiThSmall } from "react-icons/ti";
 export function Theories() {
   return (
     <>
-      <main>
-        <section
-          className={theoriesStyle.theoriesContainer}
-          id="theories-section"
-        >
-          <div className={theoriesStyle.theoriesText}>
-            <h2>Theories</h2>
-            <h3>Theories</h3>
-            <p>
-              We've been looking for over 60 years. And so that leads some
-              people to say we must be alone. The reality is, though, we have
-              just begun the search. I mean, we've looked at a few tens of
-              thousands of stars, and there are 400 billion stars in our galaxy
-              alone. Billions of galaxies in the universe. So I think we just
-              need to keep on looking. When you put it that way, it actually
-              isn't that surprising, is it? <br /> <br />I mean, we are still
-              discovering species on our own planet today. At METI, we switch
-              the process, and instead of just listening for signals, we send
-              powerful, intentional signals to other stars in the hope of
-              getting a reply. What do you say to people who go, "Hold on, "we
-              should not be alerting any life out there to our presence. "It's
-              just not worth the risk. In fact, it's irresponsible." I would say
-              it is too late. The horse is out of the barn. We have been
-              announcing our presence to the universe since the beginning of
-              radio and television. Any civilization that has the ability to
-              travel between the stars already picked up I Love Lucy.
-              <br />
-              <br />
-              The Arecibo message, sent from the Arecibo Observatory by a group
-              of scientists in Puerto Rico in 1974, was a three-minute message
-              of exactly 1,679 binary digits, arranged into 73 lines of 23
-              characters per line (these are both prime numbers and may help the
-              aliens decode the message). Written with the assistance of Carl
-              Sagan, the message itself could be arranged in a rectangular grid
-              of 0s and 1s to form a pictograph representing us.
-            </p>
-          </div>
-          <div className={theoriesStyle.theoriesImage}>
-            <div className={theoriesStyle.arecibo}>
-              <AreciboMessage></AreciboMessage>
-            </div>
-          </div>
-        </section>
-        <div className={theoriesStyle.areciboPost}>
-          <TiThSmallOutline />
-          <p className={theoriesStyle.post}>
-            The code shows fundamental facts of mathematics, human DNA, planet
-            earth's place in the solar system, and a picture of a human-like
-            figure as well as an image of the telescope itself. The Arecibo
-            Message was aimed towards the globular star cluster Messier 13,
-            25,000 light years away, in an attempt to contact extraterrestrial
-            intelligence.
+      <section
+        className={theoriesStyle.theoriesContainer}
+        id="theories-section"
+      >
+        <div className={theoriesStyle.theoriesText}>
+          <h2>Theories</h2>
+          <p>
+            We've been looking for over 60 years. And so that leads some people
+            to say we must be alone. The reality is, though, we have just begun
+            the search. I mean, we've looked at a few tens of thousands of
+            stars, and there are 400 billion stars in our galaxy alone. Billions
+            of galaxies in the universe. So I think we just need to keep on
+            looking. When you put it that way, it actually isn't that
+            surprising, is it? <br /> <br />I mean, we are still discovering
+            species on our own planet today. At METI, we switch the process, and
+            instead of just listening for signals, we send powerful, intentional
+            signals to other stars in the hope of getting a reply. What do you
+            say to people who go, "Hold on, "we should not be alerting any life
+            out there to our presence. "It's just not worth the risk. In fact,
+            it's irresponsible." I would say it is too late. The horse is out of
+            the barn. We have been announcing our presence to the universe since
+            the beginning of radio and television. Any civilization that has the
+            ability to travel between the stars already picked up I Love Lucy.
+            <br />
+            <br />
+            The Arecibo message, sent from the Arecibo Observatory by a group of
+            scientists in Puerto Rico in 1974, was a three-minute message of
+            exactly 1,679 binary digits, arranged into 73 lines of 23 characters
+            per line (these are both prime numbers and may help the aliens
+            decode the message). Written with the assistance of Carl Sagan, the
+            message itself could be arranged in a rectangular grid of 0s and 1s
+            to form a pictograph representing us.
           </p>
-          <TiThSmall />
         </div>
-      </main>
+        <div className={theoriesStyle.theoriesImage}>
+          <div className={theoriesStyle.arecibo}>
+            <AreciboMessage></AreciboMessage>
+          </div>
+        </div>
+      </section>
+      <div className={theoriesStyle.areciboPost}>
+        <TiThSmallOutline />
+        <p className={theoriesStyle.post}>
+          The code shows fundamental facts of mathematics, human DNA, planet
+          earth's place in the solar system, and a picture of a human-like
+          figure as well as an image of the telescope itself. The Arecibo
+          Message was aimed towards the globular star cluster Messier 13, 25,000
+          light years away, in an attempt to contact extraterrestrial
+          intelligence.
+        </p>
+        <TiThSmall />
+      </div>
     </>
   );
 }
